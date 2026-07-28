7bb16b4dc70aa584a67ff23d914286942616efda
isFernandes <iago.stosfernandes@gmail.com>
2026-07-28 14:52:27 -0300

    fix(deps): configure vite esbuild minifier and revert router to fix build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    cssMinify: false,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
